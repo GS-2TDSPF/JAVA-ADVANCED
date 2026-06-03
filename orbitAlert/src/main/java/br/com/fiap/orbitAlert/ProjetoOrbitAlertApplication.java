724e927562a8bb323a99e6a203c6bfd5e4d335d3
Waidemannm <mjrmolinillo@icloud.com>
2026-06-02 22:18:33 -0300

    Arrumando projeto

--- orbitAlert/src/main/java/br/com/fiap/orbitAlert/ProjetoOrbitAlertApplication.java
+++ orbitAlert/src/main/java/br/com/fiap/orbitAlert/ProjetoOrbitAlertApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableCaching
-@ComponentScan
 @EntityScan
 @EnableJpaRepositories
 @SpringBootApplication
